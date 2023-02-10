401f0231dd4d33c7b7a4563bafadae157c8679ff
AMARSILLE <antoine.marsille@ynov.com>
2023-02-10 20:00:18 +0100

    test python + subtract

--- simplemath.py
+++ simplemath.py
@@ -1,3 +1,5 @@
 class SimpleMath:
     def addition(a, b):
         return a + b
+    def substraction(a,b):
+        return a - b
--- test_simplemath.py
+++ test_simplemath.py
@@ -5,3 +5,6 @@ class TestSimpleMath(unittest.TestCase):
     def test_addition(self):
         result = SimpleMath.addition(33, 36)
         self.assertEqual(result, 69)
+    def test_substraction(self):
+        result = SimpleMath.substraction(45, 6)
+        self.assertEqual(result, 39)
